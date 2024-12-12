06edb6674d7427a92a191df17cb5a33805258dc3
Pavel <vasev-2020@bk.ru>
2024-12-12 23:40:04 +0300

    добавил наведение, паузу

--- src/components/AvatarCanvas.tsx
+++ src/components/AvatarCanvas.tsx
@@ -1,57 +1,106 @@
 import { Html, OrbitControls, Preload, useAnimations, useGLTF } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import React, { Suspense, useEffect, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import React, { Suspense, useEffect, useRef, useState } from "react";
 import { MyLoader } from "./MyLoader";
 
-const Avatar = ({buttonOnModel, rotation}) => {
-    const [index, setIndex] = useState(1);
+const Avatar = ({buttonOnModel, pause, setPause, animationMode}) => {
+    const [isAnimation, setIsAnimation] = useState(false);
     const [isClicked, setIsClicked] = useState(false);
     const [position, setPosition] = useState(0);
     const avatar = useGLTF('/VasKis7.glb');
-    const { actions, names } = useAnimations(avatar.animations, avatar.scene);
+    const animation = useAnimations(avatar.animations, avatar.scene);
+    const ref = useRef(null)
+    let color = {}
 
     useEffect(() => {
-        if (index % 2 === 1) {
-            actions['1 Вращение_2Action.001']?.reset().fadeIn(0.5).stop();
-        actions['2 Копия_2Action.001']?.reset().fadeIn(0.5).stop();
-        actions['3 Деталь_2 (Front_grb (Внутренний))Action.001']?.reset().fadeIn(0.5).stop();
-        actions['4 Сглаживание_2Action.001']?.reset().fadeIn(0.5).stop();
-        actions['5 Булева_14Action.001']?.reset().fadeIn(0.5).stop();
-        actions['6 Сглаживание_19Action.001']?.reset().fadeIn(0.5).stop();
-        actions['7 Сглаживание_18Action.001']?.reset().fadeIn(0.5).stop();
-        actions['8 Сглаживание_17Action.001']?.reset().fadeIn(0.5).stop();
-        actions['9 Сглаживание_15Action.001']?.reset().fadeIn(0.5).stop();
-        actions['10 Сглаживание_7Action.001']?.reset().fadeIn(0.5).stop();
-        actions['11 Копия_4Action.001']?.reset().fadeIn(0.5).stop();
-        actions['12 Наложение материала_1Action.003']?.reset().fadeIn(0.5).stop();
-        actions['13 Булева_16Action.001']?.reset().fadeIn(0.5).stop();
-        actions['14 Булева_17Action.001']?.reset().fadeIn(0.5).stop();
+        if (isAnimation) {
+            if (pause) {
+                animation.names.map((name) => {
+                    //@ts-ignore
+                    animation.actions[name].paused = true
+                })
+            } else {
+                animation.names.map((name) => {
+                    //@ts-ignore
+                    animation.actions[name].paused = false
+                })
+            }
+        }
+    }, [pause])
+
+    useEffect(() => {
+        if (!isAnimation) {
+            animation.names.map((name) => {
+                //@ts-ignore
+                animation.actions[name]?.reset().fadeIn(0.5).stop();
+            })
         } else {
-            actions['1 Вращение_2Action.001']?.reset().fadeIn(0.5).play();
-            actions['2 Копия_2Action.001']?.reset().fadeIn(0.5).play();
-            actions['3 Деталь_2 (Front_grb (Внутренний))Action.001']?.reset().fadeIn(0.5).play();
-            actions['4 Сглаживание_2Action.001']?.reset().fadeIn(0.5).play();
-            actions['5 Булева_14Action.001']?.reset().fadeIn(0.5).play();
-            actions['6 Сглаживание_19Action.001']?.reset().fadeIn(0.5).play();
-            actions['7 Сглаживание_18Action.001']?.reset().fadeIn(0.5).play();
-            actions['8 Сглаживание_17Action.001']?.reset().fadeIn(0.5).play();
-            actions['9 Сглаживание_15Action.001']?.reset().fadeIn(0.5).play();
-            actions['10 Сглаживание_7Action.001']?.reset().fadeIn(0.5).play();
-            actions['11 Копия_4Action.001']?.reset().fadeIn(0.5).play();
-            actions['12 Наложение материала_1Action.003']?.reset().fadeIn(0.5).play();
-            actions['13 Булева_16Action.001']?.reset().fadeIn(0.5).play();
-            actions['14 Булева_17Action.001']?.reset().fadeIn(0.5).play();
+            animation.names.map((name) => {
+                //@ts-ignore
+                animation.actions[name]?.reset().fadeIn(0.5).play();
+            })
         }
 
         return () => {
-            actions[names[index]]?.fadeOut(0.5)
+            animation.actions[animation.names[0]]?.fadeOut(0.5)
         }
-    }, [index, actions, names])
+    }, [isAnimation, animation.actions, animation.names])
+
+    const handleOver = (event, name) => {
+        const colorThis = event.object.material.color
+        if (!(name in color)) {
+            let newArr = {...color}
+            newArr[`${name}`] = colorThis.getHex()
+            color = newArr
+        }
+        event.object.material.color.set(`hotpink`)
+    }
+    const handleOut = (event, name) => {
+        event.object.material.color.setHex(color[name])
+    }
 
     return (
         <>{
             window.innerWidth >= 650
-                ? <group>
+                ? <>
+                {
+                    !animationMode ? <group
+                    ref={ref}
+                    scale={0.45}
+                    position-y={-2}
+                    rotation-y={-10}
+                    position-x={0}
+                    rotation-x={0.1}>
+                    {Object.keys(avatar?.nodes)?.map((el) => {
+                        return (<mesh 
+                        key={el}
+                        scale={avatar?.nodes?.[el]?.scale}
+                        onPointerOver={(ev) => handleOver(ev, el)}
+                        onPointerOut={(ev) => handleOut(ev, el)}
+                        onPointerLeave={(ev) => handleOut(ev, el)}
+                        geometry={avatar?.nodes?.[el]?.geometry}
+                        material={avatar?.nodes[el]?.material}
+                        position={avatar?.nodes?.[el]?.position}>
+                        </mesh>
+                    )})}
+            
+             
+                {buttonOnModel && <Html className="relative z-[10]" position={[3.7, 0.2, 0]}>
+                    <button onClick={() => {
+                        setIsAnimation((prev) => !prev)
+                        setIsClicked(!isClicked)
+                        setPosition((position + 1) % animation.names.length)
+                        setPause(false)
+                    }}
+                        className="bg-[linear-gradient(90deg,#ff3e9f,#fc4e00)] text-white font-bold z-[20] w-[100px] p-2 rounded-lg text-xs sm:text-lg sm:w-[200px] hover:bg-white hover:scale-105 duration-500 transition-all"
+                    >
+                        {
+                            !isAnimation ? 'Включить' : 'Выключить'
+                        }
+                    </button>
+                </Html>
+}
+            </group> : <group >
                     <primitive
                         object={avatar.scene}
                         scale={0.45}
@@ -59,59 +108,106 @@ const Avatar = ({buttonOnModel, rotation}) => {
                         rotation-y={-10}
                         position-x={0}
                         rotation-x={0.1}
+                        ref={ref}
                     />
+                 
                     {buttonOnModel && <Html className="relative z-[10]" position={[3.7, 0.2, 0]}>
                         <button onClick={() => {
-                            setIndex((index + 1) % names.length)
+                            setIsAnimation((prev) => !prev)
                             setIsClicked(!isClicked)
-                            setPosition((position + 1) % names.length)
+                            setPosition((position + 1) % animation.names.length)
+                            setPause(false)
                         }}
                             className="bg-[linear-gradient(90deg,#ff3e9f,#fc4e00)] text-white font-bold z-[20] w-[100px] p-2 rounded-lg text-xs sm:text-lg sm:w-[200px] hover:bg-white hover:scale-105 duration-500 transition-all"
                         >
                             {
-                                index % 2 === 1 ? 'Включить' : 'Выключить'
+                                !isAnimation ? 'Включить' : 'Выключить'
                             }
                         </button>
                     </Html>
 }
                 </group>
-                :
-                <group>
+                }
+                </> 
+                : 
+                <>
+                {
+                    !animationMode ? <group
+                    ref={ref}
+                    scale={0.45}
+                    position-y={-2}
+                    rotation-y={-10}
+                    position-x={0}
+                    rotation-x={0.1}>
+                    {Object.keys(avatar?.nodes)?.map((el) => {
+                        return (<mesh 
+                        key={el}
+                        scale={avatar?.nodes?.[el]?.scale}
+                        onPointerOver={(ev) => handleOver(ev, el)}
+                        onPointerOut={(ev) => handleOut(ev, el)}
+                        onPointerLeave={(ev) => handleOut(ev, el)}
+                        geometry={avatar?.nodes?.[el]?.geometry}
+                        material={avatar?.nodes[el]?.material}
+                        position={avatar?.nodes?.[el]?.position}>
+                        </mesh>
+                    )})}
+            
+             
+                {buttonOnModel && <Html className="relative z-[10]" position={[3.7, 0.2, 0]}>
+                    <button onClick={() => {
+                        setIsAnimation((prev) => !prev)
+                        setIsClicked(!isClicked)
+                        setPosition((position + 1) % animation.names.length)
+                        setPause(false)
+                    }}
+                        className="bg-[linear-gradient(90deg,#ff3e9f,#fc4e00)] text-white w-[100px] z-[20] font-bold p-2 rounded-lg text-xs sm:text-lg sm:w-[200px] hover:bg-white hover:scale-105 duration-500 transition-all"
+                    >
+                        {
+                            !isAnimation ? 'Включить' : 'Выключить'
+                        }
+                    </button>
+                </Html>
+}
+            </group> : <group >
                     <primitive
                         object={avatar.scene}
-                        scale={0.35}
+                        scale={0.45}
                         position-y={-2}
                         rotation-y={-10}
                         position-x={0}
                         rotation-x={0.1}
+                        ref={ref}
                     />
-                    {buttonOnModel && <Html position={[0.5, -0.5, 0]}>
+                 
+                    {buttonOnModel && <Html className="relative z-[10]" position={[3.7, 0.2, 0]}>
                         <button onClick={() => {
-                            setIndex((index + 1) % names.length)
+                            setIsAnimation((prev) => !prev)
                             setIsClicked(!isClicked)
-                            setPosition((position + 1) % names.length)
+                            setPosition((position + 1) % animation.names.length)
+                            setPause(false)
                         }}
                             className="bg-[linear-gradient(90deg,#ff3e9f,#fc4e00)] text-white w-[100px] z-[20] font-bold p-2 rounded-lg text-xs sm:text-lg sm:w-[200px] hover:bg-white hover:scale-105 duration-500 transition-all"
                         >
                             {
-                                index % 2 === 1 ? 'Включить' : 'Выключить'
+                                !isAnimation ? 'Включить' : 'Выключить'
                             }
                         </button>
-                    </Html>
-}
+                    </Html>}
                 </group>
+                }
+                </> 
         }</>
     )
 }
 
-export const AvatarCanvas = ({buttonOnModel, rotation}) => {
+export const AvatarCanvas = ({buttonOnModel, rotation, pause, setPause, animationMode}) => {
     return (
         <Canvas>
             <ambientLight intensity={0.5} />
             <pointLight position={[1, 1, 1]} />
             <OrbitControls autoRotateSpeed={0.5} autoRotate={rotation} maxDistance={12} minDistance={4} enabled={true} />
             <Suspense fallback={<MyLoader />}>
-                <Avatar  buttonOnModel={buttonOnModel} rotation={rotation} />
+                <Avatar animationMode={animationMode} setPause={setPause} buttonOnModel={buttonOnModel} pause={pause}/>
             </Suspense>
             <Preload all />
         </Canvas>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,34 +3,62 @@ import { AvatarCanvas } from "./AvatarCanvas";
 export const Hero = () => {
     const [buttonOnModel, setButtonOnModel] = useState(true)
     const [rotation, setRotation] = useState(true)
+    const [pause, setPause] = useState(false)
+    const [animationMode, setAnimationMode] = useState(true)
+
     useEffect(() => {
-        document.querySelector('#myCheckbox1').checked = buttonOnModel
-        document.querySelector('#myCheckbox2').checked = rotation
-    }, [buttonOnModel, rotation])
+        if (animationMode) {
+            //@ts-ignore
+            document.querySelector('#myCheckbox1').checked = buttonOnModel
+            //@ts-ignore
+            document.querySelector('#myCheckbox2').checked = rotation
+        }
+        
+    }, [buttonOnModel, rotation, animationMode])
     return (
         <section className="bg-[url('/hero-bck.jpg')] bg-center bg-no-repeat bg-cover relative w-full mx-auto h-screen">
             
             <div className="px-0 absolute inset-0 top-[50px] left-[50px] flex align-center max-w-7xl mx-auto gap-6">
             <div className="flex flex-col space-y-4 z-[999999000000]">
-                <div onClick={() => {
-                    console.log('asd')
-                    setButtonOnModel((prev) => !prev)
+            <div onClick={() => {
+                    setAnimationMode((prev) => {
+                        if (prev) {
+                            setButtonOnModel(false)
+                        } else {
+                            setButtonOnModel(true)
+                        }
+                        return !prev
+                })
+                    
                 }} className="flex items-center cursor-pointer z-[999999000000]">
-                    <input  type="checkbox" id='myCheckbox1' className="form-checkbox text-blue-600 h-5 w-5" />
-                    <span className="ml-2 text-white font-bold no-underline">Кнопка анимации</span>
+                    <span className="bg-[linear-gradient(90deg,#ff3e9f,#fc4e00)] text-white font-bold z-[20] w-[fit-content] p-2 rounded-lg text-xs sm:text-lg hover:bg-white hover:scale-105 duration-500 transition-all">
+                        {!animationMode ? 'Hover mode' : 'Animate mode'}
+                    </span>
                 </div>
-                
+                {animationMode && (<>
+                    <div onClick={() => setPause((prev) => !prev)} className="flex items-center cursor-pointer z-[999999000000]">
+                    <span className="bg-[linear-gradient(90deg,#ff3e9f,#fc4e00)] text-white font-bold z-[20] w-[fit-content] p-2 rounded-lg text-xs sm:text-lg hover:bg-white hover:scale-105 duration-500 transition-all">
+                        {!pause ? 'Остановить' : 'Продолжить'} анимацию
+                    </span>
+                    </div>
+                    <div onClick={() => {
+                        setButtonOnModel((prev) => !prev)
+                    }} className="flex items-center cursor-pointer z-[999999000000]">
+                        <input  type="checkbox" id='myCheckbox1' className="form-checkbox text-blue-600 h-5 w-5" />
+                        <span className="ml-2 text-white font-bold no-underline">Кнопка анимации</span>
+                    </div>
+                </>)}
                 <div onClick={() => setRotation((prev) => !prev)} className="flex items-center cursor-pointer z-[999999000000]">
                     <input  type="checkbox" id='myCheckbox2' className="form-checkbox text-blue-600 h-5 w-5" />
                     <span className="ml-2 text-white font-bold  no-underline">Автоповорот</span>
                 </div>
             </div>
                 <div>
-                    <h1 className="text-white font-extrabold flex flex-wrap text-[40px] md:text-7xl">Васев <span className="text-white ml-4"> Киселев</span></h1>
+                    <h1 className="text-white font-extrabold flex flex-wrap text-[10px] md:text-2xl">Васев <span className="text-white ml-4"> Киселев</span></h1>
                 </div>
             </div>
             <div className="relative z-10 h-screen w-screen">
-                <AvatarCanvas buttonOnModel={buttonOnModel} rotation={rotation} />
+                <AvatarCanvas animationMode={animationMode} buttonOnModel={buttonOnModel} rotation={rotation} setPause={setPause} pause={pause} />
             </div>
         </section>
     )
